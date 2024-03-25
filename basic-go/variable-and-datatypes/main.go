6b4e28e46dea5e83e349237a76b65b8eb0c30931
hudaputrasantosa <hudaputrasantosa@gmail.com>
2024-03-25 23:15:38 +0700

    add: data type and constanta

--- basic-go/variable-and-datatypes/main.go
+++ basic-go/variable-and-datatypes/main.go
@@ -2,7 +2,9 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
+	// ====================VARIABEL====================================================================
+
 	var nama string = "Huda"
 	var umur uint = 22
 
@@ -17,7 +19,7 @@ func main(){
 	var umur, tahunLahir int
 	umur, tahunLahir = 22, 2002
 
-	fmt.Println("\nHalo, namaku ", firstname, lastname,", umurku", umur ," lahir tahun", tahunLahir)
+	fmt.Println("\nHalo, namaku ", firstname, lastname, ", umurku", umur, " lahir tahun", tahunLahir)
 
 	// underscore variable sebagai variabel yang tidak perlu dipanggil (keranjang sampah)
 	_ = "Saya sedang malas"
@@ -28,11 +30,37 @@ func main(){
 	//membuat variabel pointer dengan tipe data tertentu
 	myHobbie := new(string)
 	*myHobbie = "Game"
-	
+
 	fmt.Printf("\nHobiku %s", myHobbie)
 	fmt.Printf("\nHobiku %s", *myHobbie)
-	
+
 	*myHobbie = "Lari"
 	fmt.Printf("\nHobiku %s", *myHobbie)
 
+	// ====================TIPE DATA====================================================================
+
+	var positifNumber uint8 = 255
+	var negatifNumber int8 = -100
+	decimal := 2.66
+	var exist bool = true
+	boolCondition := 5 <= 2
+	const bigNumber uint = 9999
+	// cant replace value
+	// bigNumber = 10000
+
+	fmt.Printf("bilangan positif : %d\n", positifNumber)
+	fmt.Printf("bilangan negatif : %d\n", negatifNumber)
+	fmt.Printf("bilangan decimal : %f\n", decimal)
+	fmt.Printf("bilangan decimal bulat : %.3f\n", decimal)
+	fmt.Printf("boolean exist : %t\n", exist)
+	fmt.Printf("boolean condition 5 <= 2? %t\n", boolCondition)
+	fmt.Printf("Big Number : %d\n", bigNumber)
+
+	//multi constanta
+	const (
+		years = 2002
+		today  = "senin"
+		myNumber uint = 10
+		isRain bool = false
+	)
 }
